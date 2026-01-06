1ad098f0a4bc564fb1b158e22242268a677bd019
Min <qWer79790922@gmail.com>
2026-01-06 16:51:51 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// 合併後的設定：只會有一個 export default
 export default defineConfig({
   plugins: [react()],
   base: '/contract-app/', // 這裡保留你的倉庫路徑
